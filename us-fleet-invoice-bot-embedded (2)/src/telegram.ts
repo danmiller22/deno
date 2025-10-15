6cdaa9ce91ab96451540348259e762ae394a11ee
Mueller <ajdarn3@gmail.com>
2025-10-15 14:37:58 -0500

    Update telegram.ts

--- us-fleet-invoice-bot-embedded (2)/src/telegram.ts
+++ us-fleet-invoice-bot-embedded (2)/src/telegram.ts
@@ -1,8 +1,7 @@
-import { Bot } from "https://deno.land/x/grammy@v1.25.4/mod.ts";
+import { Bot } from "https://deno.land/x/grammy@v1.25.1/mod.ts";
 
 const token = Deno.env.get("TELEGRAM_TOKEN");
 if (!token) throw new Error("Missing TELEGRAM_TOKEN");
 
 export const bot = new Bot(token);
-
 bot.command("start", (ctx) => ctx.reply("Bot ready"));
