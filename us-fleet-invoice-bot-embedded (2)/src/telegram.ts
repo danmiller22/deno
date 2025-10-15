77e5d5050ee6cd8d0c3f75d8f71b078b8a2df57c
Mueller <ajdarn3@gmail.com>
2025-10-15 04:42:32 -0500

    Update telegram.ts

--- us-fleet-invoice-bot-embedded (2)/src/telegram.ts
+++ us-fleet-invoice-bot-embedded (2)/src/telegram.ts
@@ -2,7 +2,7 @@ import { Bot, webhookCallback } from "https://deno.land/x/grammy@v1.19.1/mod.ts"
 import { TELEGRAM_TOKEN } from "./config.ts";
 import { startFlow, onText, onFile } from "./flow.ts";
 
-export type TgCtx = Parameters<Bot["on"]>[1] extends (h: (ctx: infer C)=>any)=>any ? C : any;
+export type TgCtx = Parameters<Bot["on"]>[1] extends (h:(ctx:infer C)=>any)=>any ? C : any;
 
 export const bot = new Bot<TgCtx>(TELEGRAM_TOKEN);
 bot.command("start", startFlow);
@@ -10,5 +10,4 @@ bot.hears(/^new entry$/i, startFlow);
 bot.on("message:text", onText);
 bot.on(["message:photo","message:document","message:video"], onFile);
 
-// webhook handler for Deno.serve
 export const answer = webhookCallback(bot, "std/http");
